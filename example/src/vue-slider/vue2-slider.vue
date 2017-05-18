b5eb65040e869d314a174ec7b419817126c72930
NightCatSama <642163903@qq.com>
2017-05-18 20:36:38 +0800

    modify bug

--- example/src/vue-slider/vue2-slider.vue
+++ example/src/vue-slider/vue2-slider.vue
@@ -489,8 +489,11 @@ export default {
 			}
 		},
 		setValue(val, noCb, speed) {
+			console.log(val)
 			if (this.isDiff(this.val, val)) {
 				let resetVal = this.limitValue(val)
+				console.log(resetVal)
+				console.log('=====')
 				if (resetVal !== false) {
 					this.val = this.isRange ? resetVal.concat() : resetVal
 				}
@@ -566,6 +569,10 @@ export default {
 			}
 		},
 		limitValue(val) {
+			if (this.data) {
+				return val
+			}
+			
 			let bool = false
 			if (this.isRange) {
 				val = val.map((v) => {
