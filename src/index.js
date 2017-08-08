f84ff1fc56aa03b42f96ab2937e57f6cb43ef490
Walker Leite <walker@incuca.com.br>
2017-08-08 15:51:40 -0300

    add plugin support for production use

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 var vueSlider = require('./vue2-slider')
 
+// Install by default if included from script tag
+if (typeof window !== 'undefined' && window.Vue) {
+  function install (Vue) {
+    Vue.component('vue-slider', vueSlider);
+  }
+
+  window.Vue.use(install)
+}
+
 module.exports = vueSlider
