4e922171e2ef3be06be54dfb4faed44e4bf97268
NightCatSama <642163903@qq.com>
2019-02-28 18:34:51 +0800

    test: add simple test

---
+++ tests/unit/props.spec.ts
@@ -0,0 +1,13 @@
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+import VueSlider from '~/lib'
+
+describe('Props', () => {
+  it('value', () => {
+    const wrapper = shallowMount(VueSlider, {
+      propsData: { value: 50 },
+    })
+    expect(wrapper.vm.value).to.equal(50)
+    expect(wrapper.vm.dots[0].pos).to.equal(50)
+  })
+})
