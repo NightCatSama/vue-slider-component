943eb8145553b409c7e8085b2effd669286da5fa
NightCatSama <642163903@qq.com>
2019-08-13 11:04:28 +0800

    feat: Export mark component and dot component

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,5 @@
 import VueSlider from './vue-slider'
+export { default as VueSliderMark } from './vue-slider-mark'
+export { default as VueSliderDot } from './vue-slider-dot'
 export { ERROR_TYPE } from './utils/control'
 export default VueSlider
